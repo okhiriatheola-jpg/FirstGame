76ed026f9b02d5dabb63ce307bf11e822363cfd4
okhriatheola-jpg <okhiria.theola@gmail.com>
2026-08-07 17:24:42 -0400

    Worked on Tkinter practice

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,4 +1,10 @@
 import tkinter as tk
 root = tk.Tk()
 root.title("Test")
+tk.Label(root, text="Hello, Tkinter!").pack()
+def button_clicked():
+    print("Button clicked!")
+
+tk.Button(root, text="Click me!", command=button_clicked).pack()
+name = entry_box.get()
 root.mainloop()

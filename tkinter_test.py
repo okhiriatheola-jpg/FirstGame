fe487f21054db2bdfa527995ca52a6205563a0d7
okhiriathola-jpg <okhiria.theola@gmail.com>
2026-08-08 14:17:38 -0400

    Added name input and validation

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,13 +1,24 @@
 import tkinter as tk
 root = tk.Tk()
 root.title("Test")
-tk.Label(root, text="Hello, Tkinter!").config(font=("Arial", 16)).pack(pady=10)
+
+label = tk.Label(root, text="Hello, Tkinter!")
+label.config(font=("Arial", 16))
+label.pack(pady=10)
+
 def button_clicked():
-    print("Button clicked!")
+    name = entry_box.get()
+    label.config(text=f"Hello, {name}!")
+
+    if entry_box.get() == "":
+        label.config(text="Please enter your name.")
+    else:
+        label.config(text=f"Hello, {name}!")    
 
 tk.Button(root, text="Click me!", command=button_clicked).pack()
 entry_box = tk.Entry(root)
 entry_box.pack()
-name = entry_box.get()
+
+
 
 root.mainloop()

426a5ee994b472dde039f9d6630e7b458d489a5b
okhriatheola-jpg <okhiria.theola@gmail.com>
2026-08-09 13:52:21 -0400

    done it

--- entry_stringvar.py
+++ entry_stringvar.py
@@ -4,11 +4,12 @@ root.title("entry_stringvar Test")
 name = tk.StringVar()
 entry = tk.Entry(root, textvariable=name).pack()
 def show_name():
-    print(name.get())
-    label.config(text=f"Hello, {name.get()}!")
+    if name.get() == "":
+        label.config(text="Please enter your name.")
+    else:
+        label.config(text=f"Hello, {name.get()}!")    
 label = tk.Label(root, text="Hello!")
 label.pack()    
 button = tk.Button(root, text="your name", command=show_name)
 button.pack()
-
 root.mainloop()
